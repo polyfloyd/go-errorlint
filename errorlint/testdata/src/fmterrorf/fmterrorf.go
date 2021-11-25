2f9e31541642c1e644e2efdd4743d5562c2e3d16
Bruno Rafael <brunorafael92@hotmail.com>
2021-11-25 02:00:40 -0300

    add testcase for wrapping custom error on fmt.Errorf

--- errorlint/testdata/src/fmterrorf/fmterrorf.go
+++ errorlint/testdata/src/fmterrorf/fmterrorf.go
@@ -27,6 +27,13 @@ func ErrorAtLeastOneWrap() error {
 	return fmt.Errorf("%v, %w, %v", err1, err2, err3)
 }
 
+func ErrorAtLeastOneWrapWithCustomError() error {
+	err1 := errors.New("oops1")
+	err2 := MyError{}
+	err3 := errors.New("oops3")
+	return fmt.Errorf("%v, %w, %v", err1, err2, err3)
+}
+
 func ErrorStringFormat() error {
 	err := errors.New("oops")
 	return fmt.Errorf("error: %s", err.Error()) // want "non-wrapping format verb for fmt.Errorf. Use `%w` to format errors"
