c5cc3fb88616633de60593a57851e0cb7469b700
psydvl <43755002+psydvl@users.noreply.github.com>
2024-02-03 01:57:25 +0300

    test: Add becnhmark for isAllowedErrAndFunc func

--- errorlint/allowed_test.go
+++ errorlint/allowed_test.go
@@ -40,3 +40,47 @@ func Test_isAllowedErrAndFunc(t *testing.T) {
 		})
 	}
 }
+
+func Benchmark_isAllowedErrAndFunc(b *testing.B) {
+	var benchCases = []struct {
+		desc string
+		err  string
+		fun  string
+	}{
+		{
+			desc: "empty",
+			err:  "",
+			fun:  "",
+		},
+		{
+			desc: "short",
+			err:  "x",
+			fun:  "x",
+		},
+		{
+			desc: "long not existed",
+			// should pass strings.HasPrefix length check, 30 symbols here
+			err: "xxxx_xxxx_yyyy_yyyy_zzzz_zzzz_",
+			fun: "xxxx_xxxx_yyyy_yyyy_zzzz_zzzz_",
+		},
+		{
+			desc: "existed, not wildcard",
+			err:  "io.EOF",
+			fun:  "(io.Reader).Read",
+		},
+		{
+			desc: "existed, wildcard",
+			err:  "golang.org/x/sys/unix.Exx",
+			fun:  "golang.org/x/sys/unix.xxx",
+		},
+	}
+	for _, bb := range benchCases {
+		b.Run(bb.desc, func(b *testing.B) {
+			err, fun := bb.err, bb.fun
+			b.ResetTimer()
+			for i := 0; i < b.N; i++ {
+				isAllowedErrAndFunc(err, fun)
+			}
+		})
+	}
+}
